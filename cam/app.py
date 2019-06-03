280ba3ae57d89fe56878de1a289801cd129c867c
Kotaro Yabe <idolm@ster.pw>
2019-06-03 21:56:02 +0900

    typo

--- cam/app.py
+++ cam/app.py
@@ -16,17 +16,18 @@ FPS = 30
 
 print(format("start camera system", "*^32"))
 cap = cv2.VideoCapture(0)
-print("camera default config, width:  {0}".format(cap.get(3)))
-print("camera default config, height: {0}".format(cap.get(4)))
-print("camera default config, fps:    {0}".format(cap.get(5)))
+print("camera default config, width:  {0}".format(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+print("camera default config, height: {0}".format(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+print("camera default config, fps:    {0}".format(cap.get(cv2.CAP_PROP_FPS)))
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FPS)
+cap.set(cv2.CAP_PROP_FPS, FPS)
 
-print("camera set to config, width:  {0}".format(cap.get(3)))
-print("camera set to config, height: {0}".format(cap.get(4)))
-print("camera set to config, fps:    {0}".format(cap.get(5)))
+print("camera set to config, width:  {0}".format(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+print("camera set to config, height: {0}".format(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+print("camera set to config, fps:    {0}".format(cap.get(cv2.CAP_PROP_FPS)))
+print("camera set to config, fps:    {0}".format(cap.get(cv2.CAP_PROP_FOURCC)))
 
 print(format("capture start", "*^32"))
 
