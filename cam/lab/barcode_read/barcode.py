4a388c688e9c048756d411bb0cc76644f7cc63f9
Kotaro Yabe <idolm@ster.pw>
2019-06-17 04:49:07 +0900

    100倍くらい速くなった

--- cam/lab/barcode_read/barcode.py
+++ cam/lab/barcode_read/barcode.py
@@ -3,6 +3,8 @@ import cv2
 import time
 import pandas as pd
 from logging import getLogger, StreamHandler, DEBUG, INFO, WARN
+import queue
+import threading
 
 
 class BarcodeReader:
@@ -27,129 +29,186 @@ class BarcodeReader:
     guide_bar = [1, 0, 1]
     center_bar = [0, 1, 0, 1, 0]
 
+    queue = queue.Queue()
+
     def __init__(self, *, logger=None):
         self.logger = logger or getLogger(__name__)
 
+    def detectionStartbar(self, frame, diff_index, offset=0):
+        frame_size = len(frame)
+
+        if frame_size < offset + 32:
+            # 検出する最低桁数が32桁以上なので、ここで一列分の検出は終了する
+            logger.debug("1列終了")
+            return -1, -1
+
+        if len(diff_index) < 15:
+            logger.debug("バー検知個数不足")
+            return -1, -1
+
+        start_bar_start = 0
+        for i in diff_index[diff_index >= offset]:
+            # 白から黒へ
+            if frame[i] == -255:
+                logger.debug("開始ガイドバー 1本目 開始座標: {}".format(i))
+                start_bar_start = i
+                break
+
+        if start_bar_start is 0:
+            logger.debug("開始ガイドバー 1本目 未検出")
+            return -1, -1
+
+        start_bar_end = 0
+        for i in diff_index[diff_index > start_bar_start]:
+            # 黒から白へ
+            if frame[i] == 255:
+                logger.debug("開始ガイドバー 1本目 終了座標: {}".format(i))
+                start_bar_end = i
+                break
+
+        if start_bar_end is 0:
+            logger.debug("開始ガイドバー 1本目 終了点未検出")
+            return -1, -1
+
+        if start_bar_end - start_bar_start < 3:
+            logger.debug("開始ガイドバー サイズ基準値以下")
+            return -1, -1
+
+        return start_bar_end, start_bar_start
+
+    def detectBarcode(self, frame, diff_index, start_bar_end, min_bar_size, max_bar_size):
+        frame_size = len(frame)
+
+        # 開始ガイドバー1本目が見つかった状態から開始する
+        decode_bar = [1]
+        last_hit = start_bar_end
+        for i in range(start_bar_end + 1, frame_size):
+            # 色が反転した箇所
+            if frame[i] != 0:
+                length = i - last_hit
+
+                logger.debug("reverse: %s %s %s %s", i, length, frame[i],
+                             "黒→白" if frame[i] == 255.0 else "白→黒")
+                last_hit = i
+                if length < min_bar_size or length < max_bar_size * 7:
+                    logger.debug("微小領域のノイズなのでスルー")
+                    return None
+
+                for j in range(1, 7):
+                    if min_bar_size * j <= length <= max_bar_size * j:
+                        logger.debug("範囲内: %d", j)
+                        color = 1 if frame[i] == 255.0 else 0
+                        for k in range(j):
+                            decode_bar.append(color)
+                            if len(decode_bar) == 3:
+                                if not decode_bar[0:3] == [1, 0, 1]:
+                                    logger.debug("開始ガイドバー未検出")
+                                    return None
+                        break
+
+        # 最初のバーを基準に間隔を算出した結果
+        logger.debug(decode_bar)
+        logger.debug(len(decode_bar))
+        logger.debug("開始ガイドバー: %s", decode_bar[0:3])
+        if not decode_bar[0:3] == [1, 0, 1]:
+            logger.debug("開始ガイドバー未検出")
+            return None
+
+        logger.debug("1桁目: %s", decode_bar[3:9])
+        logger.debug("1桁目: %s", self.decodeBarcode(decode_bar[3:9], 0))
+
+        logger.debug("2桁目: %s", decode_bar[9:15])
+        logger.debug("2桁目: %s", self.decodeBarcode(decode_bar[9:15], 1))
+        logger.debug("センターバー: %s", decode_bar[15:20])
+
+        if not decode_bar[15:20] == [0, 1, 0, 1, 0]:
+            logger.debug("センターバー未検出")
+            return None
+
+        logger.debug("3桁目: %s", decode_bar[20:26])
+        logger.debug("3桁目: %s", self.decodeBarcode(decode_bar[20:26], 2))
+        logger.debug("4桁目: %s", decode_bar[26:32])
+        logger.debug("4桁目: %s", self.decodeBarcode(decode_bar[26:32], 3))
+        logger.debug("終了ガイドバー: %s", decode_bar[32:35])
+
+        if not decode_bar[32:35] == [1, 0, 1]:
+            logger.debug("終了ガイドバー未検出")
+            return None
+
+        return [
+            self.decodeBarcode(decode_bar[3:9], 0),
+            self.decodeBarcode(decode_bar[9:15], 1),
+            self.decodeBarcode(decode_bar[20:26], 2),
+            self.decodeBarcode(decode_bar[26:32], 3),
+        ]
+
     def readBarcode(self, frame, offset=0):
-        frameSize = len(frame)
+        frame_size = len(frame)
 
         while 1:
-            if frameSize < offset + 32:
-                # 検出する最低桁数が32桁以上なので、ここで一列分の検出は終了する
-                raise Exception("1列終了")
+            diff_index = frame.index[frame.abs() == 255]
 
-            startBarStart = 0
-            for i in range(offset, frameSize):
-                # 白から黒へ
-                if frame[0][i] == -255:
-                    logger.debug("開始ガイドバー 1本目 開始座標: {}".format(i))
-                    startBarStart = i
-                    break
+            (start_bar_end, start_bar_start) = self.detectionStartbar(frame, diff_index, offset=offset)
+            if start_bar_end is -1 or start_bar_start is -1:
+                return None
 
-            if startBarStart is 0:
-                raise Exception("開始ガイドバー 1本目 未検出")
+            start_bar_size = start_bar_end - start_bar_start
+            logger.debug("開始ガイドバー1本目 size: {}".format(start_bar_size))
 
-            startBarEnd = 0
-            for i in range(startBarStart, frameSize):
-                # 黒から白へ
-                if frame[0][i] == 255:
-                    logger.debug("開始ガイドバー 1本目 終了座標: {}".format(i))
-                    startBarEnd = i
-                    break
-
-            if startBarEnd is 0:
-                raise Exception("開始ガイドバー 1本目 終了点未検出")
-
-            startBarSize = startBarEnd - startBarStart
-            logger.debug("開始ガイドバー1本目 size: {}".format(startBarSize))
-
-            # しきい値±10%のサイズを許容する
+            # しきい値±15%のサイズを許容する
             threshold = 0.15
 
-            minBarSize = startBarSize * (1 - threshold)
-            maxBarSize = startBarSize * (1 + threshold)
+            min_bar_size = start_bar_size * (1 - threshold)
+            max_bar_size = start_bar_size * (1 + threshold)
 
-            logger.debug("minBarSize: {0:.2f}".format(minBarSize))
-            logger.debug("maxBarSize: {0:.2f}".format(maxBarSize))
+            logger.debug("min_bar_size: {0:.2f}".format(min_bar_size))
+            logger.debug("max_bar_size: {0:.2f}".format(max_bar_size))
 
+            result = self.detectBarcode(frame, diff_index, start_bar_end, min_bar_size, max_bar_size)
+
+            if result is None:
+                offset = start_bar_end
+            else:
+                return result
+
+    def decodeBarcode(self, code, num):
+        if num is 0:
+            # odd parity
+            return self.odd_parity.index(code)
+        else:
+            # even parity
+            return self.even_parity.index(code)
+
+    def worker(self):
+        while True:
+            frame = self.queue.get()
+            if frame is None:
+                break
             try:
-                decodeBar = [1]
-                lastHit = startBarEnd
-                for i in range(startBarEnd + 1, frameSize):
-                    # 色が反転した箇所
-                    if frame_d[0][i] != 0:
-                        length = i - lastHit
-
-                        logger.debug("reverse: %s %s %s %s", i, length, frame_d[0][i],
-                                     "黒→白" if frame_d[0][i] == 255.0 else "白→黒")
-                        lastHit = i
-                        for j in range(1, 7):
-                            if minBarSize * j <= length <= maxBarSize * j:
-                                logger.debug("範囲内: %d", j)
-                                color = 1 if frame_d[0][i] == 255.0 else 0
-                                for k in range(j):
-                                    decodeBar.append(color)
-                                    if len(decodeBar) == 3:
-                                        if not decodeBar[0:3] == [1, 0, 1]:
-                                            raise Exception("開始ガイドバー未検出")
-                                break
-
-                # 最初のバーを基準に間隔を算出した結果
-                logger.debug(decodeBar)
-                logger.debug(len(decodeBar))
-                logger.debug("開始ガイドバー: %s", decodeBar[0:3])
-                if not decodeBar[0:3] == [1, 0, 1]:
-                    raise Exception("開始ガイドバー未検出")
-
-                logger.debug("1桁目: %s", decodeBar[3:9])
-                logger.debug("1桁目: %s", self.decodeBarcode(decodeBar[3:9], 0))
-
-                logger.debug("2桁目: %s", decodeBar[9:15])
-                logger.debug("2桁目: %s", self.decodeBarcode(decodeBar[9:15], 1))
-                logger.debug("センターバー: %s", decodeBar[15:20])
-
-                if not decodeBar[15:20] == [0, 1, 0, 1, 0]:
-                    raise Exception("センターバー未検出")
-
-                logger.debug("3桁目: %s", decodeBar[20:26])
-                logger.debug("3桁目: %s", self.decodeBarcode(decodeBar[20:26], 2))
-                logger.debug("4桁目: %s", decodeBar[26:32])
-                logger.debug("4桁目: %s", self.decodeBarcode(decodeBar[26:32], 3))
-                logger.debug("終了ガイドバー: %s", decodeBar[32:35])
-
-                if not decodeBar[32:35] == [1, 0, 1]:
-                    raise Exception("終了ガイドバー未検出")
-
-                return [
-                    self.decodeBarcode(decodeBar[3:9], 0),
-                    self.decodeBarcode(decodeBar[9:15], 1),
-                    self.decodeBarcode(decodeBar[20:26], 2),
-                    self.decodeBarcode(decodeBar[26:32], 3),
-                ]
+                result = self.readBarcode(frame)
+                if result is not None:
+                    logger.info("read: %s", result)
             except Exception as e:
-                # logger.error("cause error: %s", e)
-                offset = startBarEnd
+                logger.error("cause error: %s", e)
                 pass
+            self.queue.task_done()
 
 
-def decodeBarcode(self, code, num):
-    if num is 0 or num is 3:
-        # odd parity
-        return self.odd_parity.index(code)
-    else:
-        # even parity
-        return self.even_parity.index(code)
+def counter(q):
+    while True:
+        logger.error("queue size: %s", q.qsize())
+        time.sleep(1)
 
 
 if __name__ == "__main__":
     logger = getLogger(__name__)
     handler = StreamHandler()
 
-    handler.setLevel(INFO)
-    logger.setLevel(INFO)
+    handler.setLevel(DEBUG)
+    logger.setLevel(DEBUG)
 
-    # handler.setLevel(DEBUG)
-    # logger.setLevel(DEBUG)
+    # handler.setLevel(INFO)
+    # logger.setLevel(INFO)
 
     logger.addHandler(handler)
     logger.propagate = False
@@ -157,22 +216,24 @@ if __name__ == "__main__":
     start = time.time()
     print("aa")
 
-    frame = cv2.imread("./tr.png")
+    frame = cv2.imread("../../ca3.png")
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
 
+    # numpyを使って転地して横方向に反転
+    frame = frame.transpose()[:, ::-1]
+
     reader = BarcodeReader(logger=logger)
 
-    for index in range(len(frame)):
-        f = pd.DataFrame(frame[index])
-        f = f.astype("int64")
-        frame_d = f.diff()
-        try:
-            readResult = reader.readBarcode(frame_d)
-            logger.debug("read result: %s", readResult)
-        except Exception as e:
-            logger.error("cause error main: %s %s", index, e)
-            pass
+    f = pd.DataFrame(frame)
+    f = f.astype("int64")
+    frame_d = f.diff()
+
+    for index in range(len(frame_d.columns)):
+        logger.debug("%s本目", index)
+        result = reader.readBarcode(frame_d[index])
+        if result is not None:
+            logger.info("read: %s", result)
 
     duration = time.time() - start
     logger.info("time: {0:.4f}".format(duration * 1000) + "[ms] {0:.2f}".format(1 / duration) + "[fps]")
