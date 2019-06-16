0ec040a7efef2d40e3e0a0237fbdae1589940073
Kotaro Yabe <idolm@ster.pw>
2019-06-17 05:43:39 +0900

    2.6fpsぐらい出るようになった

--- cam/lab/barcode_read/barcode.py
+++ cam/lab/barcode_read/barcode.py
@@ -35,13 +35,6 @@ class BarcodeReader:
         self.logger = logger or getLogger(__name__)
 
     def detectionStartbar(self, frame, diff_index, offset=0):
-        frame_size = len(frame)
-
-        if frame_size < offset + 32:
-            # 検出する最低桁数が32桁以上なので、ここで一列分の検出は終了する
-            logger.debug("1列終了")
-            return -1, -1
-
         if len(diff_index) < 15:
             logger.debug("バー検知個数不足")
             return -1, -1
@@ -77,34 +70,31 @@ class BarcodeReader:
         return start_bar_end, start_bar_start
 
     def detectBarcode(self, frame, diff_index, start_bar_end, min_bar_size, max_bar_size):
-        frame_size = len(frame)
-
         # 開始ガイドバー1本目が見つかった状態から開始する
         decode_bar = [1]
         last_hit = start_bar_end
-        for i in range(start_bar_end + 1, frame_size):
+        for i in diff_index[diff_index > start_bar_end]:
             # 色が反転した箇所
-            if frame[i] != 0:
-                length = i - last_hit
+            length = i - last_hit
 
-                logger.debug("reverse: %s %s %s %s", i, length, frame[i],
-                             "黒→白" if frame[i] == 255.0 else "白→黒")
-                last_hit = i
-                if length < min_bar_size or length < max_bar_size * 7:
-                    logger.debug("微小領域のノイズなのでスルー")
-                    return None
+            logger.debug("reverse: %s %s %s %s", i, length, frame[i],
+                         "黒→白" if frame[i] == 255.0 else "白→黒")
+            last_hit = i
+            if length < min_bar_size or length < max_bar_size * 7:
+                logger.debug("微小領域のノイズなのでスルー")
+                return None
 
-                for j in range(1, 7):
-                    if min_bar_size * j <= length <= max_bar_size * j:
-                        logger.debug("範囲内: %d", j)
-                        color = 1 if frame[i] == 255.0 else 0
-                        for k in range(j):
-                            decode_bar.append(color)
-                            if len(decode_bar) == 3:
-                                if not decode_bar[0:3] == [1, 0, 1]:
-                                    logger.debug("開始ガイドバー未検出")
-                                    return None
-                        break
+            for j in range(1, 7):
+                if min_bar_size * j <= length <= max_bar_size * j:
+                    logger.debug("範囲内: %d", j)
+                    color = 1 if frame[i] == 255.0 else 0
+                    for k in range(j):
+                        decode_bar.append(color)
+                        if len(decode_bar) == 3:
+                            if not decode_bar[0:3] == [1, 0, 1]:
+                                logger.debug("開始ガイドバー未検出")
+                                return None
+                    break
 
         # 最初のバーを基準に間隔を算出した結果
         logger.debug(decode_bar)
@@ -142,12 +132,8 @@ class BarcodeReader:
             self.decodeBarcode(decode_bar[26:32], 3),
         ]
 
-    def readBarcode(self, frame, offset=0):
-        frame_size = len(frame)
-
+    def readBarcode(self, frame, diff_index, offset=0):
         while 1:
-            diff_index = frame.index[frame.abs() == 255]
-
             (start_bar_end, start_bar_start) = self.detectionStartbar(frame, diff_index, offset=offset)
             if start_bar_end is -1 or start_bar_start is -1:
                 return None
@@ -207,8 +193,8 @@ if __name__ == "__main__":
     handler.setLevel(DEBUG)
     logger.setLevel(DEBUG)
 
-    # handler.setLevel(INFO)
-    # logger.setLevel(INFO)
+    handler.setLevel(INFO)
+    logger.setLevel(INFO)
 
     logger.addHandler(handler)
     logger.propagate = False
@@ -231,7 +217,8 @@ if __name__ == "__main__":
 
     for index in range(len(frame_d.columns)):
         logger.debug("%s本目", index)
-        result = reader.readBarcode(frame_d[index])
+        diff_index = frame_d[index].index[frame_d[index].isin([-255, 255])]
+        result = reader.readBarcode(frame_d[index], diff_index)
         if result is not None:
             logger.info("read: %s", result)
 
